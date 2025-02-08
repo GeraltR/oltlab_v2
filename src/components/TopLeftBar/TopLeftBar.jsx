b5e547e08bf458b0f48d2f5a286433b448e6fc1b
GeraltR <donchichot@sielata.com.pl>
2025-02-08 20:08:10 +0100

    adding id to using move on

--- src/components/TopLeftBar/TopLeftBar.jsx
+++ src/components/TopLeftBar/TopLeftBar.jsx
@@ -3,19 +3,21 @@ import styles from "./TopLeftBar.module.css";
 
 export function TopLeftBar(props) {
   return (
-    <>
-      <div className={styles.leftBar}>
-        <NavLink
-          className={styles.topLeftBar}
-          style={{ marginLeft: `${props.marginLeft}` }}
-          to={props.linkTo}
-          id={props.name}
-          onMouseMove={props.onMouseMove}
-        >
-          <div>{props.chapter}</div>
-          <div className={styles.topLeftBarChapter}>{props.firstText}</div>
-        </NavLink>
-      </div>
-    </>
+    <div className={styles.leftBar}>
+      <NavLink
+        className={styles.topLeftBar}
+        style={{ marginLeft: `${props.marginLeft}` }}
+        to={props.linkTo}
+        id={props.name}
+        onMouseMove={props.onMouseMove}
+      >
+        <div onMouseMove={props.onMouseMove} id={props.name}>
+          {props.chapter}
+        </div>
+        <div className={styles.topLeftBarChapter} id={props.name}>
+          {props.firstText}
+        </div>
+      </NavLink>
+    </div>
   );
 }
