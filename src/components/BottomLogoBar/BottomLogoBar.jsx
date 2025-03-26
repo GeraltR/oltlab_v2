51dbd2f967807c91998de993fa5ba1c107ec5ea7
GeraltR <donchichot@sielata.com.pl>
2025-03-26 20:25:52 +0100

    change footer position and styles in mobile mode

--- src/components/BottomLogoBar/BottomLogoBar.jsx
+++ src/components/BottomLogoBar/BottomLogoBar.jsx
@@ -3,13 +3,11 @@ import styles from "./BottomLogoBar.module.css";
 export function BottomLogoBar(props) {
   return (
     <div
-      className={styles.bottomLogoBar}
-      style={{
-        position: `${
-          props.isMobileDevice && !props.isLandScape ? "absolute" : "relative"
-        }`,
-        top: `${props.isMobileDevice && !props.isLandScape ? "170%" : "0"}`,
-      }}
+      className={`${
+        props.isMobileDevice && !props.isLandScape
+          ? styles.bottomLogoBarMobile
+          : styles.bottomLogoBar
+      }`}
     >
       {props.children}
     </div>
